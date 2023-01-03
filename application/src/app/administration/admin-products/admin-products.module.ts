ec607d369b5359a14f68ac826af01de9c7d65180
Elena Prygun <elena.prygun@gmail.com>
2023-01-03 14:24:40 +0000

    ls_18_add async method,delay&spinners

--- application/src/app/administration/admin-products/admin-products.module.ts
+++ application/src/app/administration/admin-products/admin-products.module.ts
@@ -1,15 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminProductsComponent } from './admin-products.component';
-
-
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 @NgModule({
-  declarations: [
-    AdminProductsComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [AdminProductsComponent],
+  imports: [CommonModule, NgxSpinnerModule],
 })
-export class AdminProductsModule { }
+export class AdminProductsModule {}
