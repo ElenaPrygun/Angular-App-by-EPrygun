4b648ee9acf33cc662c717ddd120fe2b176af90b
Elena Prygun <elena.prygun@gmail.com>
2023-01-17 16:23:25 +0000

    ls_22_valid token

--- application/src/app/login/services/auth-interceptor.service.ts
+++ application/src/app/login/services/auth-interceptor.service.ts
@@ -11,6 +11,7 @@ import {
   HttpErrorResponse,
 } from '@angular/common/http';
 import { SessionTokenService } from './session-token.service';
+import jwt_decode from 'jwt-decode';
 
 @Injectable({
   providedIn: 'root',
@@ -28,9 +29,17 @@ export class AuthInterceptorService implements HttpInterceptor {
     let authReq = req;
     const token = this.sessionTokenService.getToken();
     if (token != null) {
-      authReq = req.clone({
-        headers: req.headers.set('Authorization', 'Bearer ' + token),
-      });
+      
+      const decodedToken:any = jwt_decode(token);
+      const expirationDate = new Date(decodedToken.exp * 1000);
+      if (expirationDate > new Date()) {
+        authReq = req.clone({
+          headers: req.headers.set('Authorization', 'Bearer ' + token),
+        });
+      } else {
+        
+        this.router.navigate(['/login']);
+      }
     }
     return next.handle(authReq);
   }
